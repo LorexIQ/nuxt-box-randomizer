3bb999ac7128a2e198cd02ab0d68ac70aab4d0f9
LorexIQ <murashko100500@gmail.com>
2024-02-29 20:20:20 +0300

    Updates

---
+++ server/chunks/app/client.manifest.mjs
@@ -0,0 +1,81 @@
+const client_manifest = {
+  "_vue.f36acd1f.Bt7uR5YD.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "vue.f36acd1f.Bt7uR5YD.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "error-404.CSytDhsv.js",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_vue.f36acd1f.Bt7uR5YD.js"
+    ],
+    "css": [
+      "error-404.CoUbADi5.css"
+    ]
+  },
+  "error-404.CoUbADi5.css": {
+    "file": "error-404.CoUbADi5.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "error-500.C-ELpfHP.js",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_vue.f36acd1f.Bt7uR5YD.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": [
+      "error-500.BXQ_YkC0.css"
+    ]
+  },
+  "error-500.BXQ_YkC0.css": {
+    "file": "error-500.BXQ_YkC0.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "entry.BQ6PA6kC.js",
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "css": [
+      "entry.Ch9o5Typ.css"
+    ]
+  },
+  "entry.Ch9o5Typ.css": {
+    "file": "entry.Ch9o5Typ.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
